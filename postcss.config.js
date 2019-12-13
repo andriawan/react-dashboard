0ffcf2481f0bb8d2f29ccae48ba1e57066aef89b
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-12-13 23:26:09 +0700

    improve postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production'
+      ? require('@fullhuman/postcss-purgecss')({
+          content: ['./**/*.html'],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
+      : [])
+  ]
 };
